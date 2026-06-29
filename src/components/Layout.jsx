e7267d5c46a31429c0361c1782eb2d3172289d2b
anasbughio <anasbughio@gmail.com>
2026-06-29 11:15:15 +0500

    routing

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,15 +4,11 @@ import Navbar from './Navbar';
 const Layout = () => {
   return (
     <div className="min-h-screen text-slate-800 bg-white">
-      {/* Navbar yahan upar hona zaroori hai */}
+
       <Navbar />
-      
-      {/* Agar ye Outlet nahi hoga, to Hero ya koi bhi page screen par nahi dikhega */}
       <main>
         <Outlet />
       </main>
-
-      {/* Footer yahan neeche lagayein taake har page par dikhe */}
       <footer className="bg-slate-900 text-white text-center py-6 mt-10">
         <p className="font-mono text-sm">© 2026 ONEPAGER. ALL RIGHTS RESERVED.</p>
       </footer>
